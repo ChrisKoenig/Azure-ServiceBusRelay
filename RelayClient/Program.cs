33b77490c08e33578be3f7cfed79f95bdaeda534
chriskoenig <chris.koenig@microsoft.com>
2012-08-23 23:59:57 -0500

    first commit

---
+++ RelayClient/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using Microsoft.ServiceBus;
+using System.Configuration;
+
+namespace RelayClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cf = new ChannelFactory<IStringReverserChannel>(
+                new NetTcpRelayBinding(),
+                new EndpointAddress(ServiceBusEnvironment.CreateServiceUri("sb", "ckdemo", "solver")));
+
+            cf.Endpoint.Behaviors.Add(new TransportClientEndpointBehavior { 
+                TokenProvider = TokenProvider.CreateSharedSecretTokenProvider(
+                                    ConfigurationManager.AppSettings["issuerName"],
+                                    ConfigurationManager.AppSettings["issuerSecret"]) 
+            });
+
+            using (var ch = cf.CreateChannel())
+            {
+                Console.WriteLine(ch.ReverseString("Chris Koenig"));
+            }
+
+        }
+    }
+
+    [ServiceContract(Namespace = "urn:chriskoenig")]
+    interface IStringReverser
+    {
+        [OperationContract]
+        string ReverseString(string input);
+    }
+
+    interface IStringReverserChannel : IStringReverser, IClientChannel { }
+
+}
---
+++ RelayServer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using Microsoft.ServiceBus;
+using System.Configuration;
+
+namespace RelayServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost sh = new ServiceHost(typeof(StringReverser));
+
+            sh.AddServiceEndpoint(
+               typeof(IStringReverser), new NetTcpBinding(),
+               "net.tcp://localhost:9358/reverse");
+
+            sh.AddServiceEndpoint(
+               typeof(IStringReverser), new NetTcpRelayBinding(),
+               ServiceBusEnvironment.CreateServiceUri("sb", "ckdemo", "solver"))
+                .Behaviors.Add(new TransportClientEndpointBehavior
+                {
+                    TokenProvider = TokenProvider.CreateSharedSecretTokenProvider(
+                    ConfigurationManager.AppSettings["issuerName"],
+                    ConfigurationManager.AppSettings["issuerSecret"])
+                });
+
+            sh.Open();
+
+            Console.WriteLine("Press ENTER to close");
+            Console.ReadLine();
+
+            sh.Close();
+        }
+    }
+
+    [ServiceContract(Namespace="urn:chriskoenig")]
+    interface IStringReverser
+    {
+        [OperationContract]
+        string ReverseString(string input);
+    }
+
+    interface IStringReverserChannel : IStringReverser, IClientChannel { }
+
+    public class StringReverser : IStringReverser
+    {
+        public string ReverseString(string input)
+        {
+            var a1 = input.ToCharArray();
+            Array.Reverse(a1);
+            var output = String.Join("", a1);
+            Console.WriteLine("{0} => {1}", input, output);
+            return output;
+        }
+    }
+}
+
